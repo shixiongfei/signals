7d45f4063219464defb9104c067d9fbffad21642
shixiongfei <xiongfei.shi@icloud.com>
2026-09-18 10:20:48 +0800

    Build types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
+/*
+ * index.ts
+ *
+ * Copyright (c) 2026 Xiongfei Shi
+ *
+ * Author: Xiongfei Shi <xiongfei.shi(a)icloud.com>
+ * License: Apache-2.0
+ *
+ * https://github.com/shixiongfei/signals
+ */
+
 import * as alien from "alien-signals";
 
 export type SignalGetterOptions = { untrack?: boolean };
