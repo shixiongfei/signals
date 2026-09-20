43b8c5c3c6ef424e378088245c0f7c78b13921d7
shixiongfei <xiongfei.shi@icloud.com>
2026-09-20 15:51:05 +0800

    Remove duplicate if

--- src/reactive.ts
+++ src/reactive.ts
@@ -399,20 +399,18 @@ export function reactive<T extends object>(target: T): T {
           const state = signalMap.get(key);
 
           if (state) {
-            if (state) {
-              if (
-                !("value" in after) ||
-                (!after.configurable && !after.writable)
-              ) {
-                trigger(state.get);
-                signalMap.delete(key);
-              } else {
-                const wrapped = wrap(after.value);
-                state.set(() => wrapped);
+            if (
+              !("value" in after) ||
+              (!after.configurable && !after.writable)
+            ) {
+              trigger(state.get);
+              signalMap.delete(key);
+            } else {
+              const wrapped = wrap(after.value);
+              state.set(() => wrapped);
 
-                if (!before) {
-                  trigger(state.get);
-                }
+              if (!before) {
+                trigger(state.get);
               }
             }
           }
